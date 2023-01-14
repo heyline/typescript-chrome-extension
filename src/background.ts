c7043ae39dc6ae4ee856105b0b7f393af68b3dbd
程守一 <2469758101@qq.com>
2023-01-15 00:08:21 +0800

    init

--- src/background.ts
+++ src/background.ts
@@ -5,21 +5,4 @@ let a = new Sync.SomeClass()
 chrome.action.onClicked.addListener((tab) => {
     console.log(a.getName())
 
-});
-
-
-// let active = false;
-//
-// function makeOrange(color: string): void {
-//     document.body.style.backgroundColor = color;
-// }
-//
-// chrome.action.onClicked.addListener((tab) => {
-//     active = !active;
-//     const color = active ? 'orange' : 'white';
-//     chrome.scripting.executeScript({
-//         target: {tabId: tab.id ? tab.id : -1},
-//         func: makeOrange,
-//         args: [color]
-//     }).then();
-// });
+})
